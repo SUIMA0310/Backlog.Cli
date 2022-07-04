ef87c1447443809a0a145fffe0eb15ef1da053ef
SUIMA <mizumacrocus@yahoo.co.jp>
2022-07-04 23:19:53 +0900

    :tada: ServiceCollectionExtensions.AddBacklogApi を追加

---
+++ Backlog.Api/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Backlog.Api.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddBacklogApi(
+        this IServiceCollection services,
+        IConfiguration configurationSection)
+    {
+        services.Configure<BacklogClientOptions>(configurationSection);
+        services.AddSingleton<BacklogClient>();
+
+        return services;
+    }
+}
