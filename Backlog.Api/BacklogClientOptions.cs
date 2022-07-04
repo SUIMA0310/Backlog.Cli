dbfbcc9e26b8d880822d77fd68e780c641b0cbe0
SUIMA <mizumacrocus@yahoo.co.jp>
2022-07-04 23:17:33 +0900

    :tada: Backlog Api Client を追加

---
+++ Backlog.Api/BacklogClientOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace Backlog.Api;
+
+public class BacklogClientOptions
+{
+    public string BaseUrl { get; set; } = "https://example.backlog.jp";
+    public string ApiKey { get; set; } = string.Empty;
+    public int ReadRateLimitCount { get; set; } = 80;
+    public int ReadRateLimitWindow { get; set; } = 10;
+}
