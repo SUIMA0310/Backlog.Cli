93affaba116078f09381822c4d336194baf16a95
SUIMA <mizumacrocus@yahoo.co.jp>
2022-07-04 23:21:20 +0900

    :tada: Issue 関連コマンドを追加
    get-title-file
    get-title

---
+++ Backlog.Cli/Program.cs
@@ -0,0 +1,34 @@
+﻿using Backlog.Api.Extensions;
+using Backlog.Cli.Commands;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
+
+namespace Backlog.Cli;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        await ConsoleApp.CreateBuilder(args)
+            .ConfigureAppConfiguration((_, configure) =>
+            {
+                configure.AddUserSecrets<Program>();
+            })
+            .ConfigureLogging((hostContext, logging) =>
+            {
+                logging.ClearProviders();
+                logging.AddNLog(
+                    new NLogLoggingConfiguration(
+                        hostContext.Configuration.GetSection("NLog")));
+            })
+            .ConfigureServices((hostContext, services) =>
+            {
+                services.AddBacklogApi(
+                    hostContext.Configuration.GetSection("Backlog"));
+            })
+            .Build()
+            .AddCommands<IssueCommands>()
+            .RunAsync();
+    }
+}
