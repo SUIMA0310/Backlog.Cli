bc69803479493fbc6dbd358805be3bc8f25a123f
SUIMA <mizumacrocus@yahoo.co.jp>
2022-07-04 23:17:33 +0900

    :tada: Backlog Api Client を追加

---
+++ Backlog.Api/Authenticators/BacklogApiKeyAuthenticator.cs
@@ -0,0 +1,23 @@
+﻿using RestSharp;
+using RestSharp.Authenticators;
+
+namespace Backlog.Api.Authenticators;
+
+internal class BacklogApiKeyAuthenticator : IAuthenticator
+{
+    private readonly string _apiKey;
+
+    public BacklogApiKeyAuthenticator(string apiKey)
+    {
+        _apiKey = apiKey ?? throw new ArgumentNullException(nameof(apiKey));
+    }
+
+    public ValueTask Authenticate(RestClient client, RestRequest request)
+    {
+        // Backlog API　認証と認可
+
+        // API Key を用いた認証
+        request.AddQueryParameter("apiKey", _apiKey, false);
+        return ValueTask.CompletedTask;
+    }
+}
---
+++ Backlog.Api/BacklogClient.cs
@@ -0,0 +1,43 @@
+﻿using System.Threading.RateLimiting;
+using Backlog.Api.Authenticators;
+using Microsoft.Extensions.Options;
+using RestSharp;
+using RestSharp.Serializers.NewtonsoftJson;
+
+namespace Backlog.Api;
+
+public partial class BacklogClient : IDisposable
+{
+    private readonly RateLimiter _readRequestLimiter;
+    private readonly RestClient _restClient;
+
+    public BacklogClient(IOptions<BacklogClientOptions> options) : this(options.Value)
+    {
+        /* do noting. */
+    }
+
+    public BacklogClient(BacklogClientOptions options)
+    {
+        _readRequestLimiter = new FixedWindowRateLimiter(
+            new FixedWindowRateLimiterOptions(
+                options.ReadRateLimitCount,
+                QueueProcessingOrder.OldestFirst,
+                int.MaxValue,
+                TimeSpan.FromSeconds(options.ReadRateLimitWindow)
+            ));
+
+        _restClient = new RestClient(new RestClientOptions(options.BaseUrl))
+        {
+            Authenticator = new BacklogApiKeyAuthenticator(options.ApiKey)
+        };
+
+        // use Newtonsoft.Json.
+        _restClient.UseNewtonsoftJson();
+    }
+
+    public void Dispose()
+    {
+        _readRequestLimiter.Dispose();
+        _restClient.Dispose();
+    }
+}
