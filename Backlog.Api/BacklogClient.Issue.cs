bd34b7a2da5894b5570b4b74e5019bd58bfad04a
SUIMA <mizumacrocus@yahoo.co.jp>
2022-07-04 23:18:35 +0900

    :tada: 課題(Issue) 取得 Api を追加

---
+++ Backlog.Api/BacklogClient.Issue.cs
@@ -0,0 +1,17 @@
+﻿using RestSharp;
+
+namespace Backlog.Api;
+
+public partial class BacklogClient
+{
+    public async Task<dynamic> GetIssueAsync(string issueIdOrKey, CancellationToken cancellationToken = default)
+    {
+        const string url = $"/api/v2/issues/{{{nameof(issueIdOrKey)}}}";
+        var request = new RestRequest(url)
+            .AddUrlSegment(nameof(issueIdOrKey), issueIdOrKey);
+
+        // rate limit control.
+        using var lease = await _readRequestLimiter.WaitAsync(cancellationToken: cancellationToken);
+        return await _restClient.GetAsync<dynamic>(request, cancellationToken) ?? throw new Exception();
+    }
+}
